dcc39dce5ef37e3c28467ace656f475e4aebbd1a
BT_3W2_11 <raymondg89@nycstudents.net>
2022-10-11 09:29:43 -0400

    fionished

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private double tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, double tipPercentage) {
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0;
+    }
+    //getter method that returns the totalBillBeforeTip instance variable
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+    }
+    //getter method that returns the tipPercentage instance variable
+    public double getTipPercentage(){
+        return tipPercentage*100;
+    }
+    //method that increments totalBillBeforeTip by cost and returns no value
+    public void addMeal(double cost){
+        totalBillBeforeTip+=cost;
+    }
+    //method that calculates and returns a double representing the total tip amount added to the bill
+    public double tipAmount(){
+        return totalBillBeforeTip*tipPercentage;
+    }
+    //method that calculates and returns a double representing the group’s total bill including tip
+    public double totalBill(){
+        return tipAmount()+totalBillBeforeTip;
+    }
+    //method that calculates and returns a double representing the per person cost before tip
+    public double perPersonCostBeforeTip(){
+        return totalBillBeforeTip/numPeople;
+    }
+    //method that calculates and returns a double representing the per person tip amount
+    public double perPersonTipAmount(){
+        return tipAmount()/numPeople;
+    }
+    //method that calculates and returns a double representing the total cost per person
+    public double perPersonTotalCost(){
+        return totalBill()/numPeople;
+    }
+
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+import java.util.Scanner;
+import java.lang.Math;
+public class TipCalculatorRunner {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the tip tip calculator!");
+        System.out.println("How many people are in your group?");
+        int numOfPeople = input.nextInt();
+        System.out.println("What's the tip amount in decimals? (0 - 1):");
+        double tipAmount = input.nextDouble();
+        TipCalculator tipCalculator = new TipCalculator(numOfPeople,tipAmount);
+        double mealCost = 0;
+        while (mealCost != -1) {
+            System.out.println("Enter a cost in dollars and cents, e.g.12.50 (-1 to end): ");
+            mealCost = input.nextDouble();
+            tipCalculator.addMeal(mealCost);
+            if (mealCost == -1){
+                tipCalculator.addMeal(1);
+            };
+        }
+
+        //rounding the variables (I searched this up on google, I don't know a simpler way to round variables
+        double totalBillBeforeTip = Math.round(tipCalculator.getTotalBillBeforeTip()*100);
+        totalBillBeforeTip /= 100;
+        double tipPercentage = Math.round(tipCalculator.getTipPercentage()*100);
+        tipPercentage /= 100;
+        double theTipAmount = Math.round(tipCalculator.tipAmount()*100);
+        theTipAmount /= 100;
+        double totalBill = Math.round(tipCalculator.totalBill()*100);
+        totalBill /= 100;
+        double personCostBeforeTip = Math.round(tipCalculator.perPersonCostBeforeTip()*100);
+        personCostBeforeTip /= 100;
+        double personTipAmount = Math.round(tipCalculator.perPersonTipAmount()*100);
+        personTipAmount /= 100;
+        double personTotalCost = Math.round(tipCalculator.perPersonTotalCost()*100);
+        personTotalCost /= 100;
+
+        System.out.println("-----------------------------");
+        System.out.println("Total Bill Before Tip: " + totalBillBeforeTip);
+        System.out.println("Tip Percentage: " + tipPercentage);
+        System.out.println("Total Tip: " + theTipAmount);
+        System.out.println("Total Bill With Tip: " + totalBill);
+        System.out.println("Per Person Cost Before Tip: " + personCostBeforeTip);
+        System.out.println("Tip Per Person: " + personTipAmount);
+        System.out.println("Total Cost Per Person: " + personTotalCost);
+
+    }
 }
